19c6a23f29e2c161bdd8bd65e62e2fa2b757b4f9
rahultiwari8 <rahtiwari@gmail.com>
2017-04-02 22:49:30 +0530

    ki

---
+++ src/tests/LoginTest.java
@@ -0,0 +1,30 @@
+package tests;
+
+import junit.framework.Assert;
+
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.rules.ErrorCollector;
+
+public class LoginTest {
+	
+	@Rule
+	public ErrorCollector errCol = new ErrorCollector();
+	
+	
+	@Test
+	public void testApp(){
+		// selenium code login
+		System.out.println("A");
+		
+		System.out.println("B");
+		
+		
+		System.out.println("Ending test");
+
+		
+		
+		
+	}
+
+}
